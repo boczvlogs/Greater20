0e7253e1acdb2f94e023912d449ed2b86a532c41
Bocz Vlogs <144637954+boczvlogs@users.noreply.github.com>
2023-09-12 09:56:12 +0800

    Create 20.cpp

---
+++ 20.cpp
@@ -0,0 +1,16 @@
+# include<iostream>
+using namespace std;
+
+main()
+{
+      int num;
+      cin>>num;
+      
+      if (num<20)
+      cout<<"the number is less than twenty";
+      
+      else
+      cout<<" Greater than twenty";
+      
+      system("pause");
+      }
